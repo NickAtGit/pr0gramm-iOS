39c8c722f2a4786c1d64aab8caae4682f6b9b803
nst <iphone5001@gmail.com>
2020-05-07 15:26:54 +0200

    "fixed" crash when sorting long comment threads

--- Pr0gramm/Pr0gramm/Detail/DetailViewModel.swift
+++ Pr0gramm/Pr0gramm/Detail/DetailViewModel.swift
@@ -77,45 +77,47 @@ class DetailViewModel {
     }
     
     private func sortComments(parentNodes: [Node<Comment>], childNodes: [Node<Comment>]) {
-        
-        let parentNodes = parentNodes
-        var childNodes = childNodes
-                        
-        if let firstChild = childNodes.first {
-            
-            let parentId = firstChild.value.parent!
-            if let parentNode = parentNodes.first(where: { $0.value.id == parentId }) {
-                firstChild.value.depth = parentNode.value.depth + 1
-                parentNode.add(child: firstChild)
-                childNodes.removeFirst()
-                sortComments(parentNodes: parentNodes, childNodes: childNodes)
-            } else {
-                //Comment is child of child
-                //Search children for parent
-                //Search also parentNodes, they may have a child already that is the parent
-                //of the current child we are looking at
-                            
-                parentNodes.forEach {
-                    if let foundNode = $0.search(id: parentId) {
-                        firstChild.value.depth = foundNode.value.depth + 1
-                        foundNode.add(child: firstChild)
-                        childNodes.removeFirst()
-                        sortComments(parentNodes: parentNodes, childNodes: childNodes)
-                    }
-                }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.001) {
+            let parentNodes = parentNodes
+            var childNodes = childNodes
+
+            if let firstChild = childNodes.first {
                 
-                childNodes.forEach {
-                    if let foundNode = $0.search(id: parentId) {
-                        firstChild.value.depth = foundNode.value.depth + 1
-                        foundNode.add(child: firstChild)
-                        childNodes.removeFirst()
-                        sortComments(parentNodes: parentNodes, childNodes: childNodes)
+                let parentId = firstChild.value.parent!
+                if let parentNode = parentNodes.first(where: { $0.value.id == parentId }) {
+                    firstChild.value.depth = parentNode.value.depth + 1
+                    parentNode.add(child: firstChild)
+                    childNodes.removeFirst()
+                    self.sortComments(parentNodes: parentNodes, childNodes: childNodes)
+                } else {
+                    //Comment is child of child
+                    //Search children for parent
+                    //Search also parentNodes, they may have a child already that is the parent
+                    //of the current child we are looking at
+                                
+                    parentNodes.forEach {
+                        if let foundNode = $0.search(id: parentId) {
+                            firstChild.value.depth = foundNode.value.depth + 1
+                            foundNode.add(child: firstChild)
+                            childNodes.removeFirst()
+                            self.sortComments(parentNodes: parentNodes, childNodes: childNodes)
+                        }
+                    }
+                    
+                    childNodes.forEach {
+                        if let foundNode = $0.search(id: parentId) {
+                            firstChild.value.depth = foundNode.value.depth + 1
+                            foundNode.add(child: firstChild)
+                            childNodes.removeFirst()
+                            self.sortComments(parentNodes: parentNodes, childNodes: childNodes)
+                        }
                     }
                 }
+            } else {
+                let sortedNodes = parentNodes.sorted { $0.value.confidence ?? 0 > $1.value.confidence ?? 0 }
+                self.convertCommentNodesToArray(nodes: sortedNodes, currentArray: [])
             }
-        } else {
-            let sortedNodes = parentNodes.sorted { $0.value.confidence ?? 0 > $1.value.confidence ?? 0 }
-            convertCommentNodesToArray(nodes: sortedNodes, currentArray: [])
         }
     }
     
