78b2a1f87123b60e49cf99122e65c4f6c4303fae
nst <iphone5001@gmail.com>
2020-05-31 02:29:04 +0200

    added badges view controller to profile

---
+++ Pr0gramm/Pr0gramm/Main/BadgesCollectionViewController.swift
@@ -0,0 +1,61 @@
+
+import UIKit
+
+class BadgesCollectionViewController: UICollectionViewController, StoryboardInitialViewController {
+    
+    var viewModel: UserInfoViewModel!
+    private var viewHeightConstraint: NSLayoutConstraint?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewHeightConstraint = view.heightAnchor.constraint(equalToConstant: 50)
+        viewHeightConstraint?.isActive = true
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 30, height: 30)
+        collectionView.collectionViewLayout = layout
+    }
+    
+    override func viewDidLayoutSubviews() {
+        viewHeightConstraint?.constant = collectionView.contentSize.height
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.userInfo?.badges.count ?? 0
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "badgeCell", for: indexPath) as! BadgeCollectionViewCell
+        cell.badge = viewModel.userInfo?.badges[indexPath.row]
+        return cell
+    }
+}
+
+class BadgeCollectionViewCell: UICollectionViewCell, UIContextMenuInteractionDelegate {
+    
+    var badge: Badges? {
+        didSet {
+            guard let badge = badge else { return }
+            imageView.downloadedFrom(link: "https://pr0gramm.com/media/badges/" + badge.image)
+        }
+    }
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        let interaction = UIContextMenuInteraction(delegate: self)
+        addInteraction(interaction)
+        isUserInteractionEnabled = true
+    }
+    
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { _ -> UIMenu? in
+            return self.createContextMenu()
+        }
+    }
+    
+    private func createContextMenu() -> UIMenu {
+        let okAction = UIAction(title: "Ok", image: UIImage(systemName: "checkmark.circle")) { _ in }
+        return UIMenu(title: badge?.description ?? "", children: [okAction])
+    }
+
+}
--- Pr0gramm/Pr0gramm/Main/UserInfoViewModel.swift
+++ Pr0gramm/Pr0gramm/Main/UserInfoViewModel.swift
@@ -5,19 +5,20 @@ class UserInfoViewModel {
     
     private let connector: Pr0grammConnector
     var userInfo: UserInfo?
-
+    var score: Int { userInfo?.user.score ?? 0 }
+    var userClass: Int { userInfo?.user.mark ?? 0 }
     var title: String? { connector.userName }
     
     init(connector: Pr0grammConnector) {
         self.connector = connector
+        loadUserInfo()
     }
     
-    func loadUserInfo(completion: @escaping (Bool) -> Void) {
+    private func loadUserInfo() {
         connector.fetchUserInfo { userInfo in
-            guard let userInfo = userInfo else { completion(false); return }
+            guard let userInfo = userInfo else { return }
             DispatchQueue.main.async {
                 self.userInfo = userInfo
-                completion(true)
             }
         }
     }
